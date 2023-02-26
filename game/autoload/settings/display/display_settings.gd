c68237e328440ce02f0de5957f0187412f8877ec
Adam Scott <ascott.ca@gmail.com>
2023-02-26 16:44:06 -0500

    Add bordeless and resizable settings

--- game/autoload/settings/display/display_settings.gd
+++ game/autoload/settings/display/display_settings.gd
@@ -1,5 +1,13 @@
 extends Node
 
+var borderless: bool:
+	set = set_borderless,
+	get = get_borderless
+
+var resizable: bool:
+	set = set_resizable,
+	get = get_resizable
+
 var window_mode: DisplayServer.WindowMode:
 	set = set_window_mode,
 	get = get_window_mode
@@ -7,6 +15,24 @@ var window_mode: DisplayServer.WindowMode:
 var vsync_mode: DisplayServer.VSyncMode:
 	set = set_vsync_mode,
 	get = get_vsync_mode
+
+
+func set_borderless(val: bool) -> void:
+	ProjectSettings["display/window/size/borderless"] = val
+	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_BORDERLESS, val)
+
+
+func get_borderless() -> bool:
+	return DisplayServer.window_get_flag(DisplayServer.WINDOW_FLAG_BORDERLESS)
+
+
+func set_resizable(val: bool) -> void:
+	ProjectSettings["display/window/size/resizable"] = val
+	DisplayServer.window_set_flag(DisplayServer.WINDOW_FLAG_RESIZE_DISABLED, not val)
+
+
+func get_resizable() -> bool:
+	return not DisplayServer.window_get_flag(DisplayServer.WINDOW_FLAG_RESIZE_DISABLED)
 
 
 func set_window_mode(val: DisplayServer.WindowMode) -> void:
