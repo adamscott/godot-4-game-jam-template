f0a7abecd9115416dbc3541d015aa152d51b3249
Adam Scott <ascott.ca@gmail.com>
2023-02-26 16:40:06 -0500

    Add VSync mode settings

--- game/autoload/settings/display/display_settings.gd
+++ game/autoload/settings/display/display_settings.gd
@@ -4,6 +4,10 @@ var window_mode: DisplayServer.WindowMode:
 	set = set_window_mode,
 	get = get_window_mode
 
+var vsync_mode: DisplayServer.VSyncMode:
+	set = set_vsync_mode,
+	get = get_vsync_mode
+
 
 func set_window_mode(val: DisplayServer.WindowMode) -> void:
 	ProjectSettings["display/window/size/mode"] = val
@@ -12,6 +16,15 @@ func set_window_mode(val: DisplayServer.WindowMode) -> void:
 
 func get_window_mode() -> DisplayServer.WindowMode:
 	return DisplayServer.window_get_mode()
+
+
+func set_vsync_mode(val: DisplayServer.VSyncMode) -> void:
+	ProjectSettings["display/window/vsync/vsync_mode"] = val
+	DisplayServer.window_set_vsync_mode(val)
+
+
+func get_vsync_mode() -> DisplayServer.VSyncMode:
+	return DisplayServer.window_get_vsync_mode()
 
 
 func _ready() -> void:
