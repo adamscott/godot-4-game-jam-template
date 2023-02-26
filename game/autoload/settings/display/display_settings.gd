663a338e6e8df901fe0a19a1e28b695ee2bff977
Adam Scott <ascott.ca@gmail.com>
2023-02-26 16:56:48 -0500

    Add logic to display settings

--- game/autoload/settings/display/display_settings.gd
+++ game/autoload/settings/display/display_settings.gd
@@ -16,6 +16,8 @@ var vsync_mode: DisplayServer.VSyncMode:
 	set = set_vsync_mode,
 	get = get_vsync_mode
 
+var default_values: Dictionary = {}
+
 
 func set_borderless(val: bool) -> void:
 	ProjectSettings["display/window/size/borderless"] = val
@@ -53,14 +55,51 @@ func get_vsync_mode() -> DisplayServer.VSyncMode:
 	return DisplayServer.window_get_vsync_mode()
 
 
-func _ready() -> void:
+func _init() -> void:
 	init_default_values()
+
+
+func _ready() -> void:
+	init_config_default_values()
 	apply_config()
 
 
 func init_default_values() -> void:
-	pass
+	default_values["borderless"] = ProjectSettings["display/window/size/borderless"]
+	default_values["resizable"] = ProjectSettings["display/window/size/resizable"]
+	default_values["window_mode"] = ProjectSettings["display/window/size/mode"]
+	default_values["vsync_mode"] = ProjectSettings["display/window/vsync/vsync_mode"]
+
+
+func init_config_default_values() -> void:
+	Config.set_default_value("display", "borderless", get_default_value("borderless"))
+	Config.set_default_value("display", "resizable", get_default_value("resizable"))
+	Config.set_default_value("display", "window_mode", get_default_value("window_mode"))
+	Config.set_default_value("display", "vsync_mode", get_default_value("vsync_mode"))
+
+
+func revert_to_default_config() -> void:
+	Config.set_value("display", "borderless", Config.get_default_value("display", "borderless"))
+	Config.set_value("display", "resizable", Config.get_default_value("display", "resizable"))
+	Config.set_value("display", "window_mode", Config.get_default_value("display", "window_mode"))
+	Config.set_value("display", "vsync_mode", Config.get_default_value("display", "vsync_mode"))
 
 
 func apply_config() -> void:
-	pass
+	borderless = Config.get_value("display", "borderless")
+	resizable = Config.get_value("display", "resizable")
+	window_mode = Config.get_value("display", "window_mode")
+	vsync_mode = Config.get_value("display", "vsync_mode")
+
+
+func get_default_value(key: String) -> Variant:
+	match key:
+		"borderless":
+			return default_values["borderless"]
+		"resizable":
+			return default_values["resizable"]
+		"window_mode":
+			return default_values["window_mode"]
+		"vsync_mode":
+			return default_values["vsync_mode"]
+	return null
